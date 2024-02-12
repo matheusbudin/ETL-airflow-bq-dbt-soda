8f1f345870017702b9503721b5417bf85c3d3919
matheusbudin <matheusbudin@hotmail.com>
2024-02-12 18:49:09 -0300

    feat/ added chain() for dags order configuration

--- dags/retail.py
+++ dags/retail.py
@@ -4,6 +4,7 @@ from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesyste
 from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateEmptyDatasetOperator
 from astro import sql as aql
 from astro.files import File
+from airflow.models.baseoperator import chain
 from astro.sql.table import Table, Metadata
 from astro.constants import FileType
 from airflow.operators.python import PythonOperator
@@ -87,7 +88,7 @@ def retail():
 
         return check(scan_name, checks_subpath)
     
-    check_load()
+    #check_load()
     # Set task dependencies
     #detect_and_convert_encoding >> upload_csv_to_gcs >> create_retail_dataset  >> gcs_to_raw >> check_load
     transform = DbtTaskGroup(
@@ -104,7 +105,7 @@ def retail():
         from include.soda.check_function import check
 
         return check(scan_name, checks_subpath)
-    check_transform()
+    #check_transform()
 
     report = DbtTaskGroup(
         group_id='report',
@@ -121,7 +122,19 @@ def retail():
         from include.soda.check_function import check
 
         return check(scan_name, checks_subpath)
-    check_report()
+    #check_report()
+
+    chain(
+        detect_and_convert_encoding,
+        upload_csv_to_gcs,
+        create_retail_dataset,
+        gcs_to_raw,
+        check_load(),
+        transform,
+        check_transform(),
+        report,
+        check_report(),
+    )
 retail()
 
     
