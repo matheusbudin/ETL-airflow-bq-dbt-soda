659a7ca8162a7f6d9fab80504b8dee3c035a8bce
matheusbudin <matheusbudin@hotmail.com>
2024-02-12 18:42:58 -0300

    feat/ added the task in the dag for quality check on reports sql files

--- dags/retail.py
+++ dags/retail.py
@@ -115,6 +115,13 @@ def retail():
             select=['path:models/report']
         )
     )
+
+    @task.external_python(python='/usr/local/airflow/soda_venv/bin/python')
+    def check_report(scan_name='check_report', checks_subpath='report'):
+        from include.soda.check_function import check
+
+        return check(scan_name, checks_subpath)
+    check_report()
 retail()
 
     
