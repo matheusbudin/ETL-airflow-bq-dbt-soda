2869adf44fb6e126c6ef427a297c8a282ab57ccf
matheusbudin <matheusbudin@hotmail.com>
2024-02-11 23:02:32 -0300

    feat/ added a version that has: convertion to utf-8 econding for the csv

---
+++ dags/retail_convertion_utf8.py
@@ -0,0 +1,73 @@
+from airflow.decorators import dag, task
+from datetime import datetime
+from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesystemToGCSOperator
+from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateEmptyDatasetOperator
+from astro import sql as aql
+from astro.files import File
+from astro.sql.table import Table, Metadata
+from astro.constants import FileType
+from airflow.operators.python import PythonOperator
+import os
+
+@dag(
+  start_date=datetime(2024, 1, 1),
+  schedule=None,
+  catchup=False,
+  tags=['retail'],
+)
+def retail():
+    input_csv_path = '/usr/local/airflow/include/dataset/online_retail.csv'
+    output_csv_path = '/usr/local/airflow/include/dataset/online_retail_utf8.csv'
+
+    def convert_to_utf8():
+        print(f"Converting {input_csv_path} to UTF-8")
+
+        with open(input_csv_path, 'r', encoding='latin1') as file:
+            data = file.read()
+
+        with open(output_csv_path, 'w', encoding='utf-8', newline='') as file:
+            file.write(data)
+
+        print(f"Conversion complete. Output file: {output_csv_path}")
+
+    upload_csv_to_gcs = LocalFilesystemToGCSOperator(
+        task_id='upload_csv_to_gcs',
+        src=input_csv_path,
+        dst='raw/online_retail.csv',
+        bucket='matheusrbudin-airflow-retail-project',
+        gcp_conn_id='gcp',
+        mime_type='text/csv'
+    )
+
+    create_retail_dataset = BigQueryCreateEmptyDatasetOperator(
+        task_id='create_retail_dataset',
+        dataset_id='retail',
+        gcp_conn_id='gcp',
+    )
+
+    convert_encoding = PythonOperator(
+        task_id='convert_encoding',
+        python_callable=convert_to_utf8
+    )
+
+    gcs_to_raw = aql.load_file(
+        task_id='gcs_to_raw',
+        input_file=File(
+            'gs://matheusrbudin-airflow-retail-project/raw/online_retail.csv',
+            conn_id='gcp',
+            filetype=FileType.CSV,
+        ),
+        output_table=Table(
+            name='raw_invoices',
+            conn_id='gcp',
+            metadata=Metadata(schema='retail')
+        ),
+        use_native_support=False,
+    )
+
+    # Set task dependencies
+    upload_csv_to_gcs >> create_retail_dataset
+    upload_csv_to_gcs >> convert_encoding
+    convert_encoding >> gcs_to_raw
+
+retail()
